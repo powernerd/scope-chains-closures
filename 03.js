c6ecf4dbc20aae89e373ab54b80c7cbdda720726
@powernerd <powernerdsoftware@gmail.com>
2017-07-26 16:30:00 -0700

    Showing my work from nodeschool.io

---
+++ 03.js
@@ -0,0 +1,7 @@
+function foo() {
+  var bar
+  quux = 'some value'
+  function zip() {
+    var quux = ''
+  }
+}
---
+++ 04.js
@@ -0,0 +1,9 @@
+function foo() {
+  var bar
+  quux = 'some value'
+  function zip() {
+    var quux = ''
+    bar = true
+  }
+  return zip
+}
